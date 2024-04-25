91e91266d772f0354adc8319d6146dc4ccd89167
MinhHien1999 <minhhien852123456@gmail.com>
2024-04-26 06:56:54 +0700

    fix bug

--- src/routes/app.route.js
+++ src/routes/app.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/signup", appMiddleware.validateSignUpForm, appController.SignUp);
 router.post("/login", appMiddleware.validateLoginForm, appController.login);
 router.post("/mark", appMiddleware.verifyToken, appController.addLibrary);
-router.get("/user/library",appMiddleware.verifyToken, appController.getAllLibrary);
+router.get("/user/library", appController.getAllLibrary);
 router.get("/library/:user_id", appController.getAllLibrary);
 router.get("/library/:user_id/:anime_id", appController.getAnimeLibraryById);
 router.delete("/mark/:library_id", appController.deleteAnimeLibrary);
